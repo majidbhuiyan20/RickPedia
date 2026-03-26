b386c3ecafd5969a3388357872fe42b88a942da1
Majid Bhuiyan <majid.bhuiyan2021@gmail.com>
2026-03-26 20:58:50 +0600

    init project

---
+++ lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RickPediaApp extends StatelessWidget {
+  const RickPediaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:rickpedia/app.dart';
+
+void main() {
+  runApp(const RickPediaApp());
+}
+
